63255d4d7ff340e869b33fa453d179eebd6691a4
Leonardo Velarde <lg.velarde.andrade@gmail.com>
2019-09-23 20:31:53 -0500

    Estructura de la rest

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify
+from config import db_config 
+from flask_restful import Resource, Api
+from resources.generator_url import Url
+from db import db 
 
 app = Flask(__name__)
+api = Api( app )
+db.init_app( app )
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
-@app.route('/')
-def index():
-    return jsonify({'message': 'Hello, World!'})
-
+api.add_resource(Url, '/')
 
 
 if __name__ == '__main__':
