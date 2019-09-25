401f4f6246a2888c086cbd2758760b6c16323612
Leonardo Velarde <lg.velarde.andrade@gmail.com>
2019-09-24 21:47:04 -0500

    save session and load urls

--- frontend/src/app/pages/urls/urls.component.ts
+++ frontend/src/app/pages/urls/urls.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
 import { LocalstorageService } from 'src/app/services/localstorage.service';
 import { Router } from '@angular/router';
+import { UrlService } from 'src/app/services/url.service';
 
 @Component({
   selector: 'app-urls',
@@ -14,12 +15,20 @@ export class UrlsComponent implements OnInit {
   email_login :string = ''
   password_login:string = ''
   error:string = ''
+  urls : Array<any> = []
+  domain: string = 'http://localhost:5000/u/'
   constructor(
     private loginService : LoginService,
     private localStorage : LocalstorageService,
+    private urlService : UrlService,
     private router : Router 
   ) {
     this.login = this.localStorage.jwt !== '' ? false : true
+    this.urlService.getAllUrls()
+    .subscribe(
+      data => this.loadData( data ),
+      error => this.error = error.error.message
+    )
    }
 
   ngOnInit() {
@@ -40,6 +49,15 @@ export class UrlsComponent implements OnInit {
   loadUser( data ){
     this.localStorage.saveJWT( data.token )
     this.login = false
+  }
+
+  loadData( data ){
+    console.log( data.data  )
+    this.urls = data.data 
+  }
+
+  open( url ){
+    window.open(this.domain + url ,'_blank')
   }
 
 }
